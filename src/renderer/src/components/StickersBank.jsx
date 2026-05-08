368d3026c04cbc2267217b6dc11fd181c2f09a56
SharpeurNes <sharpeurnes@gmail.com>
2026-05-08 19:18:44 +0200

    debut bank stickers

---
+++ src/renderer/src/components/StickersBank.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './StickersBank.css';
+
+const StickersBank = ({null}) => {
+    
+
+    return (
+        <div className="StickersBank">
+            
+        </div>
+    );
+};
+
+export default StickersBank;
